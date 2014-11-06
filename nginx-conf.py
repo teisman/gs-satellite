175d403df446cd78675844ab9a8c4e5153b5216a
Mark Teisman <mark@censvs.com>
2014-11-06 14:56:29 +0100

    Create ./sites-enabled if it doesn't exist already

--- nginx-conf.py
+++ nginx-conf.py
@@ -7,11 +7,11 @@ from jinja2 import Environment, PackageLoader
 domains = glob.glob('pages/*')
 domains = [os.path.split(domain)[1] for domain in domains]
 
-FOLDER = './sites-enabled/'
+PATH = './sites-enabled/'
 
 def do_clear():
-    for fpath in os.listdir(FOLDER):
-        fpath = os.path.join(FOLDER, fpath)
+    for fpath in os.listdir(PATH):
+        fpath = os.path.join(PATH, fpath)
         try:
             if os.path.isfile(fpath):
                 os.unlink(fpath)
@@ -22,12 +22,14 @@ def do_build():
     env = Environment(loader=PackageLoader(__name__, 'templates'))
     template = env.get_template('nginx.conf')
     for domain in domains:
-        fpath = os.path.join(FOLDER, domain)
+        fpath = os.path.join(PATH, domain)
         with open(fpath, 'w') as f:
             f.write(template.render(domain=domain))
 
 if __name__ == "__main__":
     if len(sys.argv) > 1 and sys.argv[1] == "build":
+        if not os.path.exists(PATH):
+            os.makedirs(PATH)
         do_clear()
         do_build()
     else:
